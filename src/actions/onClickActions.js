88c7472ec49d9ef21a4440019af7f7eb7a2cbd44
Zachary Peterson <zp.lambda@yahoo.com>
2020-08-11 19:02:27 -0400

    OnClickActions

---
+++ src/actions/onClickActions.js
@@ -0,0 +1,11 @@
+export const ADD_ONCLICK = 'ADD_ONCLICK';
+
+export const addOnClick = () => {
+    return ADD_ONCLICK;
+}
+
+export const REMOVE_ONCLICK = 'REMOVE_ONCLICK';
+
+export const removeOnClick = () => {
+    return REMOVE_ONCLICK;
+}
